1fb49b90710e5189fa4faf68209999e198233ad4
wbounas <wbounas@gmail.com>
2017-12-21 12:38:28 -0500

    Add Game Board and addMarker Function
    - Add Game Board as empty array
    - Add functioning addMarker Function
    - addMarker is limited to only valid choices (index between 0-8,
    can only add 'X' or '0')

---
+++ assets/scripts/game-engine.js
@@ -0,0 +1,32 @@
+console.log('hello world')
+
+const gameBoard = ["","","","","","","","",""]
+
+console.log(gameBoard)
+
+const addMarker = function (boardIndex, playerSymbol) {
+  if ((playerSymbol === 'X' || playerSymbol === '0') && (boardIndex >= 0 && boardIndex < 9)) {
+    gameBoard[boardIndex] = playerSymbol
+    console.log(`You selected marker ${playerSymbol} to be placed at index: ${boardIndex}.`)
+    console.log('Your game board is now:', gameBoard)
+  } else if (boardIndex >= 9) {
+    console.log(`Invalid space selection. You selected ${boardIndex}, but you cannot choose an index higher than 8. There are no more spaces after that!`)
+    console.log('The current game board is:', gameBoard)
+  } else if (playerSymbol !== 'X' || playerSymbol !== '0') {
+    console.log(`Invalid symbol selection. You selected ${playerSymbol}, but you are restricted to only X or 0.`)
+    console.log('The current game board is:', gameBoard)
+  } else {
+    console.log(`Need Valid Input! You cannot use ${playerSymbol} on space ${boardIndex}`)
+    console.log('The current game board is:', gameBoard)
+  }
+}
+
+addMarker(0, 'X')
+
+addMarker(2, 'O')
+
+addMarker(12, '3')
+
+addMarker(2, '0')
+
+addMarker(7, 'thirteen')
