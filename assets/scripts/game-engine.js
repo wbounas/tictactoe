840e21ee586a32f9d759c72f1a9c5980d50e1ca9
wbounas <wbounas@gmail.com>
2017-12-21 16:15:01 -0500

    Add checkForWin, Fix Turn Logic, Add Comments
    - Add checkForWin function that checks gameBoard array for all possible
    winning combinations
    - Move turn incrementer into addMarker function to only run if a move
    is valid. This now only will go to the next turn if the move is valid.
    - Add commentary for each major piece of code to see thought process

--- assets/scripts/game-engine.js
+++ assets/scripts/game-engine.js
@@ -1,18 +1,25 @@
+// test if JS is working
 console.log('hello world')
 
+// starting empty game board
 const gameBoard = ['', '', '', '', '', '', '', '', '']
+// console.log('Original Game Board:', gameBoard)
 
-console.log('Original Game Board:', gameBoard)
-
+// function to add a marker to the game board
 const addMarker = function (boardIndex, playerSymbol) {
   if ((playerSymbol === 'X' || playerSymbol === '0') && (boardIndex >= 0 && boardIndex < 9) && (gameBoard[boardIndex] === '')) {
+    // change the array element to the player's symbol
     gameBoard[boardIndex] = playerSymbol
+    // add turn number incrementer here to only add if a valid move
+    turnNumber++
     console.log(`You selected marker ${playerSymbol} to be placed at index: ${boardIndex}.`)
     console.log('Your game board is now:', gameBoard)
   } else if (boardIndex >= 9) {
+    // if an index parameter is greater than the size of the board array
     console.log(`Invalid space selection. You selected ${boardIndex}, but you cannot choose an index higher than 8. There are no more spaces after that!`)
     console.log('The current game board is:', gameBoard)
   } else if (gameBoard[boardIndex] !== '') {
+    // if the index parameter passed in is NOT an empty string, ie has something inside already
     console.log(`Invalid space selection. You selected ${boardIndex}, but there is already a marker placed there.`)
     console.log('The current game board is:', gameBoard)
   } else if (playerSymbol !== 'X' || playerSymbol !== '0') {
@@ -24,7 +31,7 @@ const addMarker = function (boardIndex, playerSymbol) {
   }
 }
 
-// // Test
+// // Test addMarker function
 // addMarker(0, 'X')
 // addMarker(0, '0')
 // addMarker(2, 'O')
@@ -32,65 +39,164 @@ const addMarker = function (boardIndex, playerSymbol) {
 // addMarker(2, '0')
 // addMarker(7, 'thirteen')
 
+// Check for all winning combinations (only 8)
+const checkForWin = function () {
+  if
+  ((gameBoard[0] === 'X' && gameBoard[1] === 'X' && gameBoard[2] === 'X') ||
+  (gameBoard[0] === '0' && gameBoard[1] === '0' && gameBoard[2] === '0') ||
+
+  (gameBoard[3] === 'X' && gameBoard[4] === 'X' && gameBoard[5] === 'X') ||
+  (gameBoard[3] === '0' && gameBoard[4] === '0' && gameBoard[5] === '0') ||
+
+  (gameBoard[6] === 'X' && gameBoard[7] === 'X' && gameBoard[8] === 'X') ||
+  (gameBoard[6] === '0' && gameBoard[7] === '0' && gameBoard[8] === '0') ||
+
+  (gameBoard[0] === 'X' && gameBoard[3] === 'X' && gameBoard[6] === 'X') ||
+  (gameBoard[0] === '0' && gameBoard[3] === '0' && gameBoard[6] === '0') ||
+
+  (gameBoard[1] === 'X' && gameBoard[4] === 'X' && gameBoard[7] === 'X') ||
+  (gameBoard[1] === '0' && gameBoard[4] === '0' && gameBoard[7] === '0') ||
+
+  (gameBoard[2] === 'X' && gameBoard[5] === 'X' && gameBoard[8] === 'X') ||
+  (gameBoard[2] === '0' && gameBoard[5] === '0' && gameBoard[8] === '0') ||
+
+  (gameBoard[0] === 'X' && gameBoard[4] === 'X' && gameBoard[8] === 'X') ||
+  (gameBoard[0] === '0' && gameBoard[4] === '0' && gameBoard[8] === '0') ||
+
+  (gameBoard[2] === 'X' && gameBoard[4] === 'X' && gameBoard[6] === 'X') ||
+  (gameBoard[2] === '0' && gameBoard[4] === '0' && gameBoard[6] === '0')) {
+    console.log('You have won!')
+    over = true
+    turnNumber = null
+  }
+}
+
+// Add a marker wherever Player X Chooses
 const playerXTurn = function (boardIndex) {
   addMarker(boardIndex, 'X')
 }
 
+// Add a marker wherever Player 0 Chooses
 const player0Turn = function (boardIndex) {
   addMarker(boardIndex, '0')
 }
 
-let playerTurn = 1
+// Define auxiliary Variables
+let turnNumber = 1
+let turnPlayer = ''
+let over = false
 
-if (playerTurn === 1) {
+// Turn logic
+if (turnNumber === 1) {
   // it is player_x turn
+  turnPlayer = 'Player X'
   // addMarker based on player's input
+  console.log(`It is currently turn number: ${turnNumber}. It is ${turnPlayer}'s turn.'`)
   playerXTurn(0)
-  playerTurn++
 }
-if (playerTurn === 2) {
+if (turnNumber === 2) {
   // it is player_0 turn
+  turnPlayer = 'Player 0'
   // addMarker based on player's input
+  console.log(`It is currently turn number: ${turnNumber}. It is ${turnPlayer}'s turn.'`)
   player0Turn(6)
-  playerTurn++
+  // BELOW IS OBSOLETE -- Added turn number incrementer to addMarker function
+  // if the player makes a valid move, increment the turn number
+  // otherwise, log that the player needs to pick a move!
+  // if (player0Turn() === true) {
+  //   turnNumber++
+  // } else {
+  //   console.log('Player 0 needs to pick a move!')
+  // }
 }
-if (playerTurn === 3) {
+if (turnNumber === 3) {
   // it is player_x turn
+  turnPlayer = 'Player X'
   // addMarker based on player's input
+  console.log(`It is currently turn number: ${turnNumber}. It is ${turnPlayer}'s turn.'`)
   playerXTurn(1)
-  playerTurn++
+  // BELOW IS OBSOLETE -- Added turn number incrementer to addMarker function
+  // if (playerXTurn() === true) {
+  //   turnNumber++
+  // } else {
+  //   console.log('Player X needs to pick a move!')
+  // }
 }
-if (playerTurn === 4) {
+if (turnNumber === 4) {
   // it is player_0 turn
+  turnPlayer = 'Player 0'
   // addMarker based on player's input
+  console.log(`It is currently turn number: ${turnNumber}. It is ${turnPlayer}'s turn.'`)
   player0Turn(7)
-  playerTurn++
+  // BELOW IS OBSOLETE -- Added turn number incrementer to addMarker function
+  // if (player0Turn() === true) {
+  //   turnNumber++
+  // } else {
+  //   console.log('Player 0 needs to pick a move!')
+  // }
 }
-if (playerTurn === 5) {
+if (turnNumber === 5) {
   // it is player_x turn
+  turnPlayer = 'Player X'
   // addMarker based on player's input
+  console.log(`It is currently turn number: ${turnNumber}. It is ${turnPlayer}'s turn.'`)
+  playerXTurn(5)
+  checkForWin()
+  // BELOW IS OBSOLETE -- Added turn number incrementer to addMarker function
+  // if (over === false && playerXTurn === true) {
+  //   turnNumber++
+  // } else {
+  //   console.log('Player X needs to pick a move!')
+  // }
+}
+if (turnNumber === 6) {
+  // it is player_0 turn
+  turnPlayer = 'Player 0'
+  // addMarker based on player's input
+  console.log(`It is currently turn number: ${turnNumber}. It is ${turnPlayer}'s turn.'`)
+  player0Turn(4)
+  checkForWin()
+  // BELOW IS OBSOLETE -- Added turn number incrementer to addMarker function
+  // if (over === false && player0Turn === true) {
+  //   turnNumber++
+  // } else {
+  //   console.log('Player 0 needs to pick a move!')
+  // }
+}
+if (turnNumber === 7) {
+  // it is player_x turn
+  turnPlayer = 'Player X'
+  // addMarker based on player's input
+  console.log(`It is currently turn number: ${turnNumber}. It is ${turnPlayer}'s turn.'`)
   playerXTurn(2)
-  playerTurn++
+  checkForWin()
+  // BELOW IS OBSOLETE -- Added turn number incrementer to addMarker function
+  // if (over === false && playerXTurn === true) {
+  //   turnNumber++
+  // } else {
+  //   console.log('Player X needs to pick a move!')
+  // }
 }
-if (playerTurn === 6) {
+if (turnNumber === 8) {
   // it is player_0 turn
+  turnPlayer = 'Player 0'
   // addMarker based on player's input
-  player0Turn(8)
-  playerTurn++
+  console.log(`It is currently turn number: ${turnNumber}. It is ${turnPlayer}'s turn.'`)
+  checkForWin()
+  // BELOW IS OBSOLETE -- Added turn number incrementer to addMarker function
+  // if (over === false && player0Turn === true) {
+  //   turnNumber++
+  // } else {
+  //   console.log('Player 0 needs to pick a move!')
+  // }
 }
-if (playerTurn === 7) {
+if (turnNumber === 9) {
   // it is player_x turn
+  turnPlayer = 'Player X'
   // addMarker based on player's input
-  playerTurn++
+  console.log(`It is currently turn number: ${turnNumber}. It is ${turnPlayer}'s turn.'`)
+  checkForWin()
 }
-if (playerTurn === 8) {
-  // it is player_0 turn
-  // addMarker based on player's input
-  playerTurn++
-}
-if (playerTurn === 9) {
-  // it is player_x turn
-  // addMarker based on player's input
-  playerTurn++
-}
-console.log(playerTurn)
+
+console.log('turnNumber is:', turnNumber)
+console.log('over is:', over)
