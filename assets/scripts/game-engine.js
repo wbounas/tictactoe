e4de879ad6788600ca8e671a502dc917198a1ccf
wbounas <wbounas@gmail.com>
2017-12-21 13:10:44 -0500

    Add playerXTurn, player0Turn, playerTurn, and turn logic
    - Add playerXTurn, which takes a user's choice of index and places
    an 'X' there
    - Add player0Turn, which takes a user's choice of index and places
    an '0' there
    - Add playerTurn, which starts at 1
    - Add if statements based on what the value of playerTurn is.
    - Turns 1, 3, 5, 7, and 9 will always be player_X's turn, as they will
    always start first
    - Turns 2, 4, 6, 8 will always be player_0

--- assets/scripts/game-engine.js
+++ assets/scripts/game-engine.js
@@ -2,7 +2,7 @@ console.log('hello world')
 
 const gameBoard = ["","","","","","","","",""]
 
-console.log(gameBoard)
+console.log('Original Game Board:', gameBoard)
 
 const addMarker = function (boardIndex, playerSymbol) {
   if ((playerSymbol === 'X' || playerSymbol === '0') && (boardIndex >= 0 && boardIndex < 9) && (gameBoard[boardIndex] === "")) {
@@ -13,7 +13,7 @@ const addMarker = function (boardIndex, playerSymbol) {
     console.log(`Invalid space selection. You selected ${boardIndex}, but you cannot choose an index higher than 8. There are no more spaces after that!`)
     console.log('The current game board is:', gameBoard)
   } else if (gameBoard[boardIndex] !== "") {
-    console.log(`Invalid space seelection. You selected ${boardIndex}, but there is already a marker placed there.`)
+    console.log(`Invalid space selection. You selected ${boardIndex}, but there is already a marker placed there.`)
     console.log('The current game board is:', gameBoard)
   } else if (playerSymbol !== 'X' || playerSymbol !== '0') {
     console.log(`Invalid symbol selection. You selected ${playerSymbol}, but you are restricted to only X or 0.`)
@@ -23,6 +23,69 @@ const addMarker = function (boardIndex, playerSymbol) {
     console.log('The current game board is:', gameBoard)
   }
 }
+
+const playerXTurn = function (boardIndex) {
+  addMarker(boardIndex, 'X')
+}
+
+const player0Turn = function (boardIndex) {
+  addMarker(boardIndex, '0')
+}
+
+let playerTurn = 1
+
+if (playerTurn === 1) {
+  // it is player_x turn
+  // addMarker based on player's input
+  playerXTurn(0)
+  playerTurn++
+}
+if (playerTurn === 2) {
+  // it is player_0 turn
+  // addMarker based on player's input
+  player0Turn(6)
+  playerTurn++
+}
+if (playerTurn === 3) {
+  // it is player_x turn
+  // addMarker based on player's input
+  playerXTurn(1)
+  playerTurn++
+}
+if (playerTurn === 4) {
+  // it is player_0 turn
+  // addMarker based on player's input
+  player0Turn(7)
+  playerTurn++
+}
+if (playerTurn === 5) {
+  // it is player_x turn
+  // addMarker based on player's input
+  playerXTurn(2)
+  playerTurn++
+}
+if (playerTurn === 6) {
+  // it is player_0 turn
+  // addMarker based on player's input
+  player0Turn(8)
+  playerTurn++
+}
+if (playerTurn === 7) {
+  // it is player_x turn
+  // addMarker based on player's input
+  playerTurn++
+}
+if (playerTurn === 8) {
+  // it is player_0 turn
+  // addMarker based on player's input
+  playerTurn++
+}
+if (playerTurn === 9) {
+  // it is player_x turn
+  // addMarker based on player's input
+  playerTurn++
+}
+console.log(playerTurn)
 
 // // Test
 // addMarker(0, 'X')
