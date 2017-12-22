6c62a40d8ac7252ed407328c342c6d0ebc2605b4
wbounas <wbounas@gmail.com>
2017-12-22 17:41:19 -0500

    Add Tic-Tac-Toe Grid, Rename Right-Footer-Box
    - Utilize HTML table to form Tic-Tac-Toe Grid
    - Rename right-footer-box and right-footer-text to footer-box
    and footer-text

--- assets/scripts/auth/events.js
+++ assets/scripts/auth/events.js
@@ -3,7 +3,7 @@
 const getFormFields = require('../../../lib/get-form-fields')
 const api = require('./api')
 const ui = require('./ui')
-const store = require('../store')
+// const store = require('../store')
 
 const onSignUp = function (event) {
   const data = getFormFields(this)
