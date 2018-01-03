e1fa5cd0f36b3d9219e07cd9d579d529f81b0b17
wbounas <wbounas@gmail.com>
2018-01-02 23:06:04 -0500

    Add curl script to GET Games that are Over
    - Tested and verified working curl script to retrieve past games
    - Can modify curl script for ?over=true or ?over=false to see if the
    game is over or not

--- scripts/game-api/get-games-over.sh
+++ scripts/game-api/get-games-over.sh
@@ -2,10 +2,10 @@
 
 # sh scripts/auth/sign-in.sh
 # EMAIL=wb PASSWORD=wb1
-# sh scripts/game-api/get-games.sh
+# sh scripts/game-api/get-games-over.sh
 # TOKEN="token"
 
-curl "http://tic-tac-toe.wdibos.com/games" \
+curl "http://tic-tac-toe.wdibos.com/games?over=true" \
   --include \
   --request GET \
   --header "Content-Type: application/json" \
